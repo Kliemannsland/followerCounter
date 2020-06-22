ddcbd3848e834ad82492e3a9256efc8ba7c554e7
Christian Figge <christian@frozendonkey.com>
2020-06-22 08:43:06 +0000

    fixing instagram-api whith neon-JS way

--- app/Http/Controllers/InstagramController.php
+++ app/Http/Controllers/InstagramController.php
@@ -11,25 +11,30 @@ class InstagramController extends Controller
     public function show()
     {
         //Cache data for 15 minutes
-        Cache::forget('follower_instagram');
         $follower = Cache::remember('follower_instagram', 15, function() {
-            $count = 0;
             $guzzle = new Client();
+            $response = $guzzle->request('GET', 'https://www.instagram.com/' . config('social.instagram_id') . '/?__a=1');
+            $content = $response->getBody()->getContents();
+            if(strlen($content) < 1) return 0;
+            $arr = json_decode($content, true);
+            if (empty($arr)) return 0;
 
+            if (empty($arr['graphql']['user']['edge_followed_by']['count'])) return 0;
+            return intval($arr['graphql']['user']['edge_followed_by']['count']);
+
+
+            /*Just another workaround, if the json_api way above is broken
             $response = $guzzle->request('GET',
-                'https://www.instagram.com/' .config('social.instagram_id')
+            'https://www.instagram.com/' .config('social.instagram_id')
             );
 
             $content = $response->getBody()->getContents();
 
             if (strlen($content) > 0) {
                 preg_match('/edge_followed_by\":{\"count\":(\d*)/', $content, $matches);
-                if(is_array($matches) && isset($matches[1])) {
-                    return intval($matches[1]);
-                }
-            }
-
-            return $count;
+                if(is_array($matches) && isset($matches[1])) return intval($matches[1]);
+                return 0;
+            }*/
         });
 
         return $follower;
