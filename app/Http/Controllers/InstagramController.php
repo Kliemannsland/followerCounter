13646b2ffd31d63bfe12f942cf4a61c5c1e3865d
Christian Figge <christian@frozendonkey.com>
2020-06-22 08:20:42 +0000

    implemented new hacky way to read insta-followers

--- app/Http/Controllers/InstagramController.php
+++ app/Http/Controllers/InstagramController.php
@@ -11,35 +11,21 @@ class InstagramController extends Controller
     public function show()
     {
         //Cache data for 15 minutes
+        Cache::forget('follower_instagram');
         $follower = Cache::remember('follower_instagram', 15, function() {
             $count = 0;
             $guzzle = new Client();
 
             $response = $guzzle->request('GET',
-                'https://www.instagram.com/web/search/topsearch/', [
-                    'query' => [
-                        'query' => config('social.instagram_id')
-                    ]
-                ]);
+                'https://www.instagram.com/' .config('social.instagram_id')
+            );
 
             $content = $response->getBody()->getContents();
 
             if (strlen($content) > 0) {
-                $arr = json_decode($content, true);
-                if (isset($arr['users']) && !empty($arr['users'])) {
-                    $users = $arr['users'];
-
-                    foreach ($users AS $v) {
-                        if (isset($v['user'])) {
-                            $user = $v['user'];
-                            if (isset($user['username']) && $user['username'] == config('social.instagram_id')) {
-                                if (isset($user['follower_count'])) {
-                                    $count = (int) $user['follower_count'];
-                                    break;
-                                }
-                            }
-                        }
-                    }
+                preg_match('/edge_followed_by\":{\"count\":(\d*)/', $content, $matches);
+                if(is_array($matches) && isset($matches[1])) {
+                    return intval($matches[1]);
                 }
             }
 
